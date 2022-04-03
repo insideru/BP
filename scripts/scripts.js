fb9a999e245dc03d40b67550a47238c0d15a25d4
Insideru <insideru@gmail.com>
2022-04-03 16:13:18 +0300

    add pontaj

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -495,6 +495,25 @@ function validateConcediu () {
   }
 }
 
+function getVars () {
+  var $_GET = {};
+    if(document.location.toString().indexOf('?') !== -1) {
+        var query = document.location
+                      .toString()
+                      // get the query string
+                      .replace(/^.*?\?/, '')
+                      // and remove any existing hash string (thanks, @vrijdenker)
+                      .replace(/#.*$/, '')
+                      .split('&');
+
+        for(var i=0, l=query.length; i<l; i++) {
+          var aux = decodeURIComponent(query[i]).split('=');
+          $_GET[aux[0]] = aux[1];
+        }
+    }
+  return $_GET;  
+}
+
 function getNoDaysOff(data1, data2) {
   var zileLibere = 0;
   data1.setHours(0, 0, 0);
@@ -593,6 +612,10 @@ $.ajax({
         //
     }
 });
+}
+
+function ponteazaWithDate(date) {
+  console.log(date);
 }
 
 function initCalendar() {
