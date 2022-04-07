7642d8512bca3914c7874a25490a02f58008e774
Insideru <insideru@gmail.com>
2022-04-08 02:21:33 +0300

    fix

--- test.php
+++ test.php
@@ -8,4 +8,12 @@ require_once './include/db.php';
     console.log(guidCookie);
     var guidTOid = "<?php echo getIDfromGUID($_COOKIE["userGUID"]); ?>";
     console.log(guidTOid);
+
+    function getCookie(cookiename) 
+        {
+        // Get name followed by anything except a semicolon
+        var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
+        // Return everything after the equal sign, or an empty string if the cookie name not found
+        return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
+        }
 </script>
