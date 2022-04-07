d1e28a9b85777e5e07b9752d03204a2588fe4cf8
Insideru <insideru@gmail.com>
2022-04-08 02:08:56 +0300

    tests

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -702,6 +702,7 @@ function initCalendar() {
       //dataType    : 'json', // what type of data do we expect back from the server
       encode      : true,
       success     : function(data) {
+        console.log(data);
         rvdData = JSON.parse(data);
         if (typeof(rvdData) === 'object') {
           buildTimesheetCalendarEvents(rvdData.timesheets);
