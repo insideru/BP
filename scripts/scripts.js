d31652193cf95ee57c62b3f81cc129992cce1875
Insideru <insideru@gmail.com>
2022-04-08 02:19:47 +0300

    cookie fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -799,3 +799,11 @@ function validateAdduser () {
   });
   //
 }
+
+function getCookie(cookiename) 
+  {
+  // Get name followed by anything except a semicolon
+  var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
+  // Return everything after the equal sign, or an empty string if the cookie name not found
+  return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
+  }
--- test.php
+++ test.php
@@ -4,8 +4,8 @@ require_once './include/elems.php';
 require_once './include/db.php';
 ?>
 <script>
-    var guidCookie = document.cookie.userGUID;
+    var guidCookie = getCookie("userGUID");
     console.log(guidCookie);
     var guidTOid = "<?php echo getIDfromGUID($_COOKIE["userGUID"]); ?>";
-    console.log(guidCookie);
+    console.log(guidTOid);
 </script>
