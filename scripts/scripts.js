d12923919ec331c47746f740eb27ef343f1b5b9c
Insideru <insideru@gmail.com>
2022-04-07 04:16:09 +0300

    fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -780,7 +780,7 @@ function validateAdduser () {
     success     : function(data) {
       console.log(data);
       rcvData = JSON.parse(data);
-      if (data.newAccount[0].substring(0, 21) == "The new account ID is") {
+      if (rcvData.newAccount['message'].substring(0, 21) == "The new account ID is") {
         //a mers
         var modalInstance = M.Modal.getInstance($('#newPontor'));
         modalInstance.close();
@@ -789,7 +789,7 @@ function validateAdduser () {
         populateCollabs();
         M.toast({html: rcvData.newAccount});
       } else {
-        $("#addUserError").html(rcvData.newAccount);
+        $("#addUserError").html(rcvData.newAccount['message']);
         return;
       }
     }
