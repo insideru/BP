070de6b73845492d6bb63abef399a42751a32231
Insideru <insideru@gmail.com>
2022-04-10 22:35:28 +0300

    test

--- test.php
+++ test.php
@@ -2,18 +2,10 @@
 require_once './include/admin_elems.php';
 require_once './include/elems.php';
 require_once './include/db.php';
-?>
-<script>
-    var guidCookie = getCookie("userGUID");
-    console.log(guidCookie);
-    var guidTOid = "<?php echo getIDfromGUID($_COOKIE["userGUID"]); ?>";
-    console.log(guidTOid);
+require_once './include/account.php';
 
-    function getCookie(cookiename) 
-        {
-        // Get name followed by anything except a semicolon
-        var cookiestring=RegExp(cookiename+"=[^;]+").exec(document.cookie);
-        // Return everything after the equal sign, or an empty string if the cookie name not found
-        return decodeURIComponent(!!cookiestring ? cookiestring.toString().replace(/^[^=]+./,"") : "");
-        }
-</script>
+$hashedToken = '$2y$10$h2Y4KSuUWv/g.E1lg21wuOt7QZGucSjZWjpb0HppreN8vGIjyznbe';
+$accountToken = '1DzMKRp22YGermY5';
+
+echo password_verify($accountToken, $hashedToken);
+?>
