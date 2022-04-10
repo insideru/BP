33be61b9b7e3650f0bc1f5e54b9857f44a82c238
Insideru <insideru@gmail.com>
2022-04-10 22:38:25 +0300

    test

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ require_once './include/db.php';
 require_once './include/account.php';
 
 $hashedToken = '$2y$10$h2Y4KSuUWv/g.E1lg21wuOt7QZGucSjZWjpb0HppreN8vGIjyznbe';
-$accountToken = '1DzMKRp22YGermY5';
+$accountToken = '1DzMKRp22YGermY4';
 
 echo password_verify($accountToken, $hashedToken);
 ?>
