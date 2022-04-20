4b4d99ec8d6d36405b25287857932551d65ec5d9
Insideru <insideru@gmail.com>
2022-04-20 23:23:16 +0300

    fix update loop

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1209,10 +1209,7 @@ function changeProjectBudget(projID, projBudget) {
 }
 
 function changeProjectDeadline () {
-  var instance = M.Datepicker.getInstance($('#newProjectDeadline'));
-  $('#newProjectDeadline').change(function() {
-    console.log($(this).val());
-  });
+  let instance = M.Datepicker.getInstance($('#newProjectDeadline'));
   instance.open();
 }
 
